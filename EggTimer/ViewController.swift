6ad9aac453b8b7b625b2302d4679138f590735f5
altos721 <altos721@gmail.com>
2024-06-01 20:56:51 +0800

    add alarm sound

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    let eggTimes: [String: Float] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    let eggTimes: [String: Float] = ["Soft": 3, "Medium": 4, "Hard": 7]
     
     var countdownTimer: Timer?
+    var audioPlayer: AVAudioPlayer?
+    
     var totalTime: Float!
     var remaingTime: Float!
     
@@ -27,33 +30,37 @@ class ViewController: UIViewController {
         totalTime = eggTimes[hardness]!
         remaingTime = eggTimes[hardness]!
         
-        
         startTimer()
         
     }
     
-    
-    
     func startTimer() {
         countdownTimer?.invalidate()
         titleLabel.text = "How do you like your eggs?"
+        audioPlayer?.stop()
+        progressBar.progress = 1
+        
+        do {
+            let soundURL = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")!
+            audioPlayer = try AVAudioPlayer(contentsOf: soundURL)
+        } catch {
+            return
+        }
         
         countdownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
-    
     @objc func updateTimer() {
         if remaingTime > 0 {
+            remaingTime -= 1
             progressBar.progress = remaingTime / totalTime
             print(remaingTime!)
-            remaingTime -= 1
         } else {
             progressBar.progress = 0
             titleLabel.text = "Done"
+            audioPlayer?.play()
             countdownTimer?.invalidate()
         }
     }
-    
-    
 
 }
