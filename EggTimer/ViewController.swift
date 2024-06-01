b82229e34f006cccf9be7873fb738b7f082bee68
altos721 <altos721@gmail.com>
2024-06-01 19:37:06 +0800

    add progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
+    let eggTimes: [String: Float] = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var countdownTimer: Timer?
+    var totalTime: Float!
+    var remaingTime: Float!
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        let hardness = sender.currentTitle!
+        
+        totalTime = eggTimes[hardness]!
+        remaingTime = eggTimes[hardness]!
+        
+        
+        startTimer()
+        
+    }
+    
+    
+    
+    func startTimer() {
+        countdownTimer?.invalidate()
+        titleLabel.text = "How do you like your eggs?"
+        
+        countdownTimer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    
+    @objc func updateTimer() {
+        if remaingTime > 0 {
+            progressBar.progress = remaingTime / totalTime
+            print(remaingTime!)
+            remaingTime -= 1
+        } else {
+            progressBar.progress = 0
+            titleLabel.text = "Done"
+            countdownTimer?.invalidate()
+        }
+    }
+    
+    
 
 }
